b72362cec3fe47190eb16da9e2dc4eb1d48cfe49
dirrbick <kory.a.jaxn@gmail.com>
2019-12-05 19:34:26 -0800

    Got chart to popluate information

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use script';
+'use strict';
 //calls pic info from html
 var picOne = document.getElementById('picture1');
 var picTwo = document.getElementById('picture2');
@@ -41,9 +41,10 @@ function hideElement(elem) {
   elem.style.display = 'none';
 }
 var previousPictures = [];
+var currentPictures = [];
 
 function imageGenerator() {
-  var currentPictures = [];
+  currentPictures = [];
   for(var i = 0; i < pictureArrayContainers.length; i ++){
     var randomIndexCurrently = indexAtRandom(pictureArray.length);
     while (currentPictures.includes(randomIndexCurrently) || currentPictures.includes(previousPictures) || previousPictures.includes(randomIndexCurrently)){
@@ -79,7 +80,50 @@ function handleClick(event) {
     console.log('I stopped');
     voteTally();
     hideElement(mainRemove);
+    showElement(populateChart());
   }
+}
+//Create chart
+var titleLabel = [];
+var viewLabel = [];
+var clicksLabel = [];
+
+
+function tableData () {
+  titleLabel = [];
+  viewLabel = [];
+  clicksLabel = [];
+  for (var i = 0 ; i < pictureArray.length; i++) {
+    titleLabel.push(pictureArray[i].title);
+    viewLabel.push(pictureArray[i].timesViewed);
+    clicksLabel.push(pictureArray[i].clicked);
+  }
+}
+function populateChart (){
+  tableData();
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+    // The data for our dataset
+    data: {
+      labels: titleLabel,
+      datasets: [{
+        label: 'Times Clicked',
+        backgroundColor: 'rgb(238,130,238)',
+        borderColor: 'rgb(0, 0, 0)',
+        data: clicksLabel,
+      },
+      {
+        label: 'Times Viewed',
+        backgroundColor: 'rgb(212, 175, 55)',
+        borderColor: 'rgb(0,0,0)',
+        data: viewLabel,
+      }],
+    },
+    // Configuration options go here
+    options: {},
+  });
 }
 
 
